ba1d54c662d3c2512f9771d9be6e3dab15ad08a0
waro0002 <quentin.waroquet@etudiant.univ-reims.fr>
2022-10-12 10:08:30 +0200

    Doctrine

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -2,80 +2,76 @@
 
 namespace App\Controller;
 
-use App\Entity\Quote;
-
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\HttpFoundation\Request;
+use App\Repository\QuoteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Quote;
+use Doctrine\Persistence\ManagerRegistry;
 
 class QuoteController extends AbstractController
 {
     #[Route('/quote', name: 'quote_index')]
-
-    public function index(ManagerRegistry $doctrine, Request $request ): Response
+    public function index(Request $request, QuoteRepository $quoteRepository): Response
     {
-        $repository = $doctrine->getRepository(quote::class);
-        $quote = $repository->findAll();
+        $queryBuilder = $quoteRepository->createQueryBuilder('q');
+
         $search = $request->query->get('search');
         if (!empty($search)) {
-            $quotes = $repository->findOneByContent('$search');
-            $quotes = array_filter($quotes, function(array $quote) use ($search) {
-                return str_contains(strtolower($quote['content']), strtolower($search));
-            });
-        }
-        else{
-            return $this->render('quote/index.html.twig', ['quotes' => $quote]);
-        }
-        if (!$quote) {
-            throw $this->createNotFoundException(
-                'No quote found'
-            );
-        }
-
-
-
-
-
-
-/*
-    public function index(Request $request): Response
-    {
-        $quotes = [
-            [
-                'content' => 'Sire, sire ! On en a gros!',
-                'meta' => 'Perceval, Livre II, Les Explotés',
-            ],
-            [
-                'content' => 'SUne pluie de pierres en intérieur donc ! Je vous prenais pour un pied de chaise mais vous êtes un précurseur en fait !',
-                'meta' => ' Élias de Kelliwic\'h, Livre IV, Le Privilégié',
-            ],
-            [
-                'content' => 'Je viens de lui mettre une balle dans la tête…… Il m’a regardé de travers.',
-                'meta' => 'Tommy Shelby',
-            ],
-            [
-                'content' => 'J’ai pénétré leur lieu d\'habitation de façon subrogative […] en tapinant.',
-                'meta' => 'Hervé de Rinel, Livre III, 91 : L’Espion',
-            ],
-            [
-                'content' => '2500 pièces d\'or ???! Eh... eh... c\'est un blague? 2500 pièces d\'or, mais ou voulez vous que j\'trouve 2500 pièces d\'or, dans l\'cul d\'une vache ?!',
-                'meta' => ' Seigneur Jacca, Livre I, 21 : La taxe militaire',
-            ]
-        ];*/
-
-        /*$search = $request->query->get('search');
-        if (!empty($search)) {
-            $quotes = array_filter($quotes, function(array $quote) use ($search) {
-                return str_contains(strtolower($quote['content']), strtolower($search));
-            });
+            $queryBuilder->where('q.content LIKE :search')->setParameter('search', '%'.$search.'%');
         }
 
         return $this->render('quote/index.html.twig', [
-            'quotes' => $quotes,
-            'search' => $search,
-        ]);*/
-
+            'quotes' => $queryBuilder->getQuery()->getResult()
+        ]);
     }
+
+    #[Route('/quote/{id}/delete', name: 'quote_delete')]
+    public function delete(ManagerRegistry $doctrine, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $quote = $entityManager->getRepository(Quote::class)->find($id);
+
+        $entityManager->remove($quote);
+        $entityManager->flush();
+        return $this->redirectToRoute('quote_index');
+    }
+
+    #[Route('/quote/{id}/edit', name: 'quote_edit')]
+    public function update(ManagerRegistry $doctrine, int $id, Request $request): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $quote = $entityManager->getRepository(Quote::class)->find($id);
+
+        if (!$quote) {
+            throw $this->createNotFoundException(
+                'Aucunne citation trouvée pour l\'identifiant '.$id
+            );
+        }
+        if ($request->isMethod('POST')){
+            $content = $request->request->get('name');
+            $meta = $request->request->get('meta');
+            $quote->setContent($content);
+            $quote->setMeta($meta);
+            $entityManager->flush();
+            return $this->redirectToRoute('quote_index');
+        }
+        return $this->render('quote/edit.html.twig',[
+            'quote' => $quote
+        ]);
+    }
+
+    #[Route('/quote/new', name: 'quote_new')]
+    public function new(Request $request, ManagerRegistry $doctrine,): Response
+    {
+        $content = $request->query->get('content');
+        $meta = $request->query->get('meta');
+
+        //$quote->setName('Nouveau nom de produit !');
+        //$entityManager->flush();
+
+        return $this->render('quote/edit.html.twig');
+    }
+
 }
